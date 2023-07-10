b7d1fbec3115a3666ca3fc0ac4cf3f337f61a88f
Jesse Chong <jessechong@pursuit.org>
2023-07-10 15:36:31 -0400

    Added css background

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const axios = require('axios');
-require("dotenv").config();
+// const axios = require('axios');
+// require("dotenv").config();
 
-(async () => {
-  try{
-const options = {
-  method: 'GET',
-  url: 'https://league-of-legends-champions.p.rapidapi.com/champions/en-us/bard',
-  headers: {
-    'X-RapidAPI-Key': process.env.RAPID_API_Key,
-    'X-RapidAPI-Host': 'league-of-legends-champions.p.rapidapi.com'
-  }
-};
+// (async () => {
+//   try{
+// const options = {
+//   method: 'GET',
+//   url: 'https://league-of-legends-champions.p.rapidapi.com/champions/en-us/bard',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.RAPID_API_Key,
+//     'X-RapidAPI-Host': 'league-of-legends-champions.p.rapidapi.com'
+//   }
+// };
 
-	const response = await axios.request(options);
-  const responseData = response.data;
+// 	const response = await axios.request(options);
+//   const responseData = response.data;
 
-const responseContainer = document.getElementById("response");
-responseContainer.innterText = JSON.stringify(responseData, null, 2);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-})();
+// const responseContainer = document.getElementById("response");
+// responseContainer.innterText = JSON.stringify(responseData, null, 2);
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// })();
 
 
